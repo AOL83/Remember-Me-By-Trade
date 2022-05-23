5dce27c521d81ea354107e0d5d91322fd80f67f8
AOL83 <terencelewis610@gmail.com>
2022-05-23 13:29:27 +0000

    Added correct input link for submit button removed js.file from directory removed js.link from header in index.html removed unneeded sections for inedx.html contact.html thankyou.html

--- assets /script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLink = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
